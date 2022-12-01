62851770ef6541da020d7f75fe767c0b9a87d67e
benjaminPicq <117632310+benjaminPicq@users.noreply.github.com>
2022-12-01 11:27:54 +0100

    Update and rename cass_.py to cas_.py

--- cas_.py
+++ cas_.py
@@ -1,7 +1,6 @@
 import pyxel
 
 # taille de la fenetre 128x128 pixels
-# ne pas modifier
 pyxel.init(128, 128, title="Nuit du c0de")
 # position initiale du vaisseau
 # (origine des positions : coin haut gauche)
@@ -24,13 +23,13 @@ def update():
     """mise à jour des variables (30 fois par seconde)"""
     global vaisseau_x
     # mise à jour de la position du vaisseau
-    vaisseau_x = vaisseau_deplacement(vaisseau_x)
+    vaisseau_x, vaisseau_y = vaisseau_deplacement(vaisseau_x, vaiiseau_y)
 
 
 def draw():
     """création des objets (30 fois par seconde)"""
     pyxel.cls(0)
-    pyxel.rect(vaisseau_x, 8, 8, 1)
+    pyxel.rect(vaisseau_x, vaisseau_y 8, 8, 1)
 
 pyxel.run(update, draw)
 
