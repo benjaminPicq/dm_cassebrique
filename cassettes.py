5e20503c23c5090cf9603038758d021d4ee9ed7d
benjaminPicq <117632310+benjaminPicq@users.noreply.github.com>
2022-12-01 11:59:32 +0100

    Update and rename cassette.py to cassettes.py

--- cassettes.py
+++ cassettes.py
@@ -8,6 +8,9 @@ pyxel.init(128, 128, title="Nuit du c0de")
 # (origine des positions : coin haut gauche)
 vaisseau_x = 60
 vaisseau_y = 100
+bloc[0]_x = 5
+bloc[0]_y = 5
+
 
 def vaisseau_deplacement(x, y):
     """déplacement avec les touches de directions"""
@@ -53,7 +56,7 @@ def draw():
     pyxel.rect(vaisseau_x, vaisseau_y, 12, 4, 3)
     
     # bloc (4x4)
-    pyxel.rect(bloc[0], bloc[1], 4, 4, 8)
+    pyxel.rect(bloc[0], 4, 4, 8)
 
 pyxel.run(update, draw)
 
