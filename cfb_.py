0e70726036cdd58d364cfc8cc1bd2e20d2abad56
benjaminPicq <117632310+benjaminPicq@users.noreply.github.com>
2022-12-03 13:36:08 +0100

    Update cfb_.py

--- cfb_.py
+++ cfb_.py
@@ -20,6 +20,20 @@ bloc4_x = 40
 bloc4_y = 5
 bloc5_x = 50
 bloc5_y = 5
+bloc6_x = 60
+bloc6_y = 5
+bloc7_x = 70
+bloc7_y = 5
+bloc8_x = 80
+bloc8_y = 5
+bloc9_x = 90
+bloc9_y = 5
+bloc10_x = 100
+bloc10_y = 5
+bloc11_x = 110
+bloc11_y = 5
+
+
 
 def plateau_deplacement(x, y):
     """déplacement avec les touches de directions"""
@@ -52,8 +66,8 @@ def draw():
     # vide la fenetre
     pyxel.cls(0)
 
-    # vaisseau (rectangle 12x4)
-    pyxel.rect(plateau_x, plateau_y, 12, 4, 3)
+    # vaisseau (rectangle 20x4)
+    pyxel.rect(plateau_x, plateau_y, 20, 4, 3)
     
     # blocs (7x7)
     pyxel.rect(bloc1_x, bloc1_y, 7, 7, 8)
@@ -61,6 +75,12 @@ def draw():
     pyxel.rect(bloc3_x, bloc3_y, 7, 7, 8)
     pyxel.rect(bloc4_x, bloc4_y, 7, 7, 8)
     pyxel.rect(bloc5_x, bloc5_y, 7, 7, 8)
+    pyxel.rect(bloc6_x, bloc6_y, 7, 7, 8)
+    pyxel.rect(bloc7_x, bloc7_y, 7, 7, 8)
+    pyxel.rect(bloc8_x, bloc8_y, 7, 7, 8)
+    pyxel.rect(bloc9_x, bloc9_y, 7, 7, 8)
+    pyxel.rect(bloc10_x, bloc10_y, 7, 7, 8)
+    pyxel.rect(bloc11_x, bloc11_y, 7, 7, 8)
 
 pyxel.run(update, draw)
 
