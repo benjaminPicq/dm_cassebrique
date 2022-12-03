edd21b459a7eb88692399a9cd8d42afd27683b04
benjaminPicq <117632310+benjaminPicq@users.noreply.github.com>
2022-12-03 13:21:41 +0100

    Update and rename cf_.py to cb_.py

--- cb_.py
+++ cb_.py
@@ -10,7 +10,8 @@ plateau_x = 60
 plateau_y = 30
 
 #initialisation blocs
-bloc_liste = []
+bloc1_x = 5
+bloc1_y = 115
 
 
 
@@ -25,24 +26,16 @@ def plateau_deplacement(x, y):
             x = x - 1
     return x, y
 
-def bloc_création(bloc_liste) : 
-    if (pyxel.frame_count % 30 == 0) :
-        bloc_liste.append([random.randint(0, 40), 0])
-    return bloc_liste
 # =========================================================
 # == UPDATE
 # =========================================================
 def update():
     """mise à jour des variables (30 fois par seconde)"""
 
-    global plateau_x, plateau_y, bloc_liste
+    global plateau_x, plateau_y
 
     # mise à jour de la position du vaisseau
     plateau_x, plateau_y = plateau_deplacement(plateau_x, plateau_y)
-    
-    #position des blocs
-    bloc_liste = bloc_creation(bloc_liste)
-
 
 # =========================================================
 # == DRAW
@@ -56,9 +49,8 @@ def draw():
     # vaisseau (carre 8x8)
     pyxel.rect(plateau_x, plateau_y, 12, 4, 3)
     
-    # blocs destructibles (3x3)
-    for bloc in bloc_liste :
-        pyxel.rect(bloc[0], bloc[1], bloc[2], 3, 3, 8)
+    # bloc 1 (3x3)
+    pyxel.rect(bloc1_x, bloc1_y, 3, 3, 8)
 
 pyxel.run(update, draw)
 
