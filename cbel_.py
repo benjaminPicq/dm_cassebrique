abf2b984581d7ac74a7728fc612e7f5eb0b2d206
benjaminPicq <117632310+benjaminPicq@users.noreply.github.com>
2022-12-04 12:44:22 +0100

    Update and rename cbe_.py to cbel_.py

--- cbel_.py
+++ cbel_.py
@@ -47,7 +47,8 @@ def draw():
     # plateau (polygone de 6 côtés 20x4)
     pyxel.rect(plateau_x, plateau_y, 20, 4, 3)
     pyxel.tri(plateau_x, plateau_y, plateau_x, plateau_y+5, plateau_x-5, plateau_y+5, 3)
-    pyxel.tri(plateau_x+20, vaisseau_y, vaisseau_x+32, vaisseau_y+15, vaisseau_x+47, vaisseau_y+15, 12)
+    pyxel.tri(plateau_x+20, vaisseau_y, vaisseau_x+20, vaisseau_y+5, vaisseau_x+25, vaisseau_y+5, 3)
+    pyxel.rect(vaisseau_x-5, vaisseau_y+5, 20, 4, 3)
     
     # balle (cercle, rayon = 3, couleur)
     pyxel.circ(balle_x, balle_y, 3, 10)
