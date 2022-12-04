64daae712e0f7bde9e6938f4158716321e4d7bc3
benjaminPicq <117632310+benjaminPicq@users.noreply.github.com>
2022-12-04 13:47:52 +0100

    Update and rename test1.py to test2.py

--- test2.py
+++ test2.py
@@ -2,24 +2,24 @@ import pyxel
 plateau_x = 128
 plateau_y = 220
 
-def plateau_deplacement(x, y):
+def plateau_deplacement(x, y) :
     """déplacement avec les touches de directions"""
 
-    if pyxel.btn(pyxel.KEY_RIGHT):
+    if pyxel.btn(pyxel.KEY_RIGHT) :
         if (x < 202) :
             x = x + 4
-    if pyxel.btn(pyxel.KEY_LEFT):
+    if pyxel.btn(pyxel.KEY_LEFT) :
         if (x > 20) :
             x = x - 4
     return x, y
   
- def update() :
+def update() :
     
     global plateau_x, plateau_y
     
     plateau_x, plateau_y = plateau_deplacement(plateau_x, plateau_y)
 
- def draw():
+def draw() :
     """création des objets (30 fois par seconde)"""
 
     # vide la fenetre
@@ -28,4 +28,4 @@ def plateau_deplacement(x, y):
     pyxel.tri(plateau_x, plateau_y, plateau_x, plateau_y+15, plateau_x-15, plateau_y+15, 3)
     pyxel.tri(plateau_x+32, plateau_y, plateau_x+32, plateau_y+15, plateau_x+47, plateau_y+15,)
     
- pyxel.run(update, draw)
+pyxel.run(update, draw)
