f684aa33a1a95cef442801b1dba2743fed6c7a09
benjaminPicq <117632310+benjaminPicq@users.noreply.github.com>
2022-12-04 09:31:32 +0100

    Update and rename cbfe_.py to cbe_.py

--- cbe_.py
+++ cbe_.py
@@ -1,4 +1,5 @@
 import pyxel
+import turtle
 
 # taille de la fenetre 128x128 pixels
 # ne pas modifier
@@ -9,6 +10,8 @@ pyxel.init(128, 128, title="Nuit du c0de")
 plateau_x = 60
 plateau_y = 90
 blocs = []
+balle_x = 60
+balle_y = 85
 
 def plateau_deplacement(x, y):
     """déplacement avec les touches de directions"""
@@ -25,18 +28,26 @@ def bloc_creation(blocs) :
     for x in range(10, 10) :
         pyxel.rect(blocs[1], 7, 7, 8)   
     return blocs
+
+def balle_creation(balle) :
+    turtle = turtle.Turtle()
+    rayon = 20
+    turtle.circle(rayon)
+    return balle
 # =========================================================
 # == UPDATE
 # =========================================================
 def update():
     """mise à jour des variables (30 fois par seconde)"""
 
-    global plateau_x, plateau_y, blocs
+    global plateau_x, plateau_y, blocs, balle_x, balle_y
 
     # mise à jour de la position du vaisseau
     plateau_x, plateau_y = plateau_deplacement(plateau_x, plateau_y)
     
     blocs = bloc_creation(blocs)
+    
+    balle_x, balle_y = balle_creation(balle_x, balle_y)
 # =========================================================
 # == DRAW
 # =========================================================
