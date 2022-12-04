ad59c7e86eaa7cb1b8f587ee8d7e0df85ef6643d
benjaminPicq <117632310+benjaminPicq@users.noreply.github.com>
2022-12-04 19:56:31 +0100

    Update and rename flo.py to flo_.py

--- flo_.py
+++ flo_.py
@@ -91,10 +91,10 @@ def draw():
     if balle_y <= 128 :
         
         # polygone de 6 cotes
-        pyxel.rect(plateau_x, plateau_y, 10, 5, 3)
-        pyxel.tri(plateau_x, plateau_y, plateau_x, plateau_y+4, plateau_x-4, plateau_y+4, 3)
-        pyxel.tri(plateau_x+10, plateau_y, plateau_x+10, plateau_y+4, plateau_x+20, plateau_y+4, 3)
-        pyxel.rect(plateau_x-4, plateau_y+4, 30, 4, 3)
+        pyxel.rect(plateau_x, plateau_y, 20, 10, 3)
+        pyxel.tri(plateau_x, plateau_y, plateau_x, plateau_y+9, plateau_x-9, plateau_y+9, 3)
+        pyxel.tri(plateau_x+20, plateau_y, plateau_x+20, plateau_y+9, plateau_x+33, plateau_y+9, 3)
+        pyxel.rect(plateau_x-9, plateau_y+9, 42, 4, 3)
     
         # premiere ligne de briques(x, y, 9x9, couleur)
         pyxel.rect(10, 5, 9, 9, 8)
