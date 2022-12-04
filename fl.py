73244c9abc13cdffb3a77606001fa7dcb8f9b772
benjaminPicq <117632310+benjaminPicq@users.noreply.github.com>
2022-12-04 19:31:03 +0100

    Update and rename teckerzz_.py to fl.py

--- fl.py
+++ fl.py
@@ -92,9 +92,9 @@ def draw():
         
         # polygone de 6 cotes
         pyxel.rect(plateau_x, plateau_y, 10, 5, 3)
-        pyxel.tri(plateau_x, plateau_y, plateau_x, plateau_y+6, plateau_x-6, plateau_y+6, 3)
-        pyxel.tri(plateau_x+10, plateau_y, plateau_x+10, plateau_y+6, plateau_x+20, plateau_y+6, 3)
-        pyxel.rect(plateau_x-6, plateau_y+6, 24.35, 4, 3)
+        pyxel.tri(plateau_x, plateau_y, plateau_x, plateau_y+4, plateau_x-4, plateau_y+4, 3)
+        pyxel.tri(plateau_x+10, plateau_y, plateau_x+10, plateau_y+4, plateau_x+20, plateau_y+4, 3)
+        pyxel.rect(plateau_x-4, plateau_y+4, 24.35, 4, 3)
     
         # premiere ligne de briques(x, y, 9x9, couleur)
         pyxel.rect(10, 5, 9, 9, 8)
