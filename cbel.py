e35640ce0663c441f4ccd61cb53f229974d13656
benjaminPicq <117632310+benjaminPicq@users.noreply.github.com>
2022-12-04 13:37:18 +0100

    Update cbel.py

--- cbel.py
+++ cbel.py
@@ -8,7 +8,6 @@ pyxel.init(128, 128, title="Nuit du c0de")
 # (origine des positions : coin haut gauche)
 plateau_x = 60
 plateau_y = 90
-blocs = []
 balle_x = 60
 balle_y = 85
 
@@ -43,14 +42,12 @@ def draw():
     pyxel.cls(0)
 
     # plateau (polygone de 6 côtés 20x4)
-    pyxel.rect(plateau_x, plateau_y, 20, 4, 3)
-    pyxel.tri(plateau_x, plateau_y, plateau_x, plateau_y+5, plateau_x-5, plateau_y+5, 3)
-    pyxel.tri(plateau_x+20, vaisseau_y, vaisseau_x+20, vaisseau_y+5, vaisseau_x+25, vaisseau_y+5, 3)
-    pyxel.rect(vaisseau_x-5, vaisseau_y+5, 20, 4, 3)
+    pyxel.rect(plateau_x, plateau_y, 32, 14, 3)
+    pyxel.tri(plateau_x, plateau_y, plateau_x, plateau_y+15, plateau_x-15, plateau_y+15, 3)
+    pyxel.tri(plateau_x+32, plateau_y, plateau_x+32, plateau_y+15, plateau_x+47, plateau_y+15,)
     
     # balle (cercle, rayon = 3, couleur)
     pyxel.circ(balle_x, balle_y, 3, 10)
     
 
 pyxel.run(update, draw)
-
