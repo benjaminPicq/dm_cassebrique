5462a8f438c2d8cb292101dd4e544984b7f56be2
benjaminPicq <117632310+benjaminPicq@users.noreply.github.com>
2022-12-11 21:19:04 +0100

    Update and rename dm_casse_brique_154.py to dm_casse_brique_155.py

--- dm_casse_brique_155.py
+++ dm_casse_brique_155.py
@@ -57,7 +57,7 @@ def balle_deplacement(x, y) :
             xballe = -xballe
 
     for n in range(0, len(nmbr_bl)) :
-        if blocsx[n] <= x <= (blocsx[n] + 9) and blocsy[n] <= y <= (blocsy[n] :
+        if blocsx[n] <= x <= (blocsx[n] + 9) and blocsy[n] <= y <= (blocsy[n] + 9) :
             blocsx.pop(n)
             blocsy.pop(n)
             c.pop(0)
