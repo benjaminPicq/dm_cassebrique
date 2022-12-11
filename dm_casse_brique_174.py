7df2be05aa266696adca5a073c6ef8acadd7c421
benjaminPicq <117632310+benjaminPicq@users.noreply.github.com>
2022-12-11 22:27:01 +0100

    Update and rename dm_casse_brique_173.py to dm_casse_brique_174.py

--- dm_casse_brique_174.py
+++ dm_casse_brique_174.py
@@ -12,8 +12,8 @@ balle_x = 60
 balle_y = 98
 
 # vitesse de la balle
-xballe = 1
-yballe = 1
+xballe = 2
+yballe = 2
 
 # coordonnes des blocs
 nmbr_bl = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21]
